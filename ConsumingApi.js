0e6bd4e362245d7eee8a8a82449b0bd9e3614d59
Thomas Charlton <tchrlton1@gmail.com>
2018-07-12 19:40:36 -0700

    Consuming API Checkpoint

---
+++ ConsumingApi.js
@@ -0,0 +1,19 @@
+let url = 'https://newsapi.org/v2/top-headlines?sources=hacker-news&apiKey=86f443bcb0974a47ac37832dfc7639e0';
+
+fetch(url)
+  .then(r => {
+    return r.json();
+  })
+  .then(data => {
+    let storyList = document.createElement('ul');
+    let body = document.querySelector('body');
+    body.appendChild(storyList);
+    data.articles.map (news => {
+      let newsItem = document.createElement('li');
+      newsItem.innerHTML = '<a href="' + news.href + '">' + news.title + "</a>";
+      storyList.appendChild(newsItem);
+    });
+   })
+  .catch(e => {
+    console.log(`An error occured: ${e}`);
+  });
